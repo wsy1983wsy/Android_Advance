b1adddfd170c54101fdde8fda276cca0849815b4
wsy <wsy1983wsy@163.com>
2020-04-25 20:31:54 +0800

    init

---
+++ 自定义view/View_Day01/app/src/main/java/com/wsy/view_day01/view/TextView.java
@@ -0,0 +1,44 @@
+package com.wsy.view_day01.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class TextView extends View {
+    // 在代码中new TextView的时候使用
+    // TextView textView = new Text(getActivity());
+    public TextView(Context context) {
+        super(context);
+    }
+
+    // 在布局中使用
+    // xml中直接使用的时候会被调用
+    //    <com.wsy.view_day01.view.TextView
+    //        android:layout_width="match_parent"
+    //        android:layout_height="wrap_content"
+    //        android:textColor="@android:color/holo_blue_dark"
+    //        android:textSize="20sp" />
+    public TextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    // 在布局中，有style时会被调用
+    //   <com.wsy.view_day01.view.TextView
+    //        style="@style/defaultTextView" />
+    public TextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    /**
+     * 自定义view的测量方法
+     *
+     * @param widthMeasureSpec
+     * @param heightMeasureSpec
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
