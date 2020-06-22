a794bde2573baaf5fe168637bbc9a0a1062e4f6d
wsy <wsy1983wsy@163.com>
2020-06-22 22:11:48 +0800

    动画测试

--- 自定义view/View_Day01/app/src/main/java/com/wsy/view_day01/activity/ColorTrackActivity.java
+++ 自定义view/View_Day01/app/src/main/java/com/wsy/view_day01/activity/ColorTrackActivity.java
@@ -1,15 +1,45 @@
 package com.wsy.view_day01.activity;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
 
 import com.wsy.view_day01.R;
+import com.wsy.view_day01.view.ColorTrackTextView;
 
 public class ColorTrackActivity extends Activity {
+    ColorTrackTextView colorTrackTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_track);
+        colorTrackTextView = findViewById(R.id.colorTrackTextView);
+    }
+
+    public void leftToRight(View view) {
+        changeColor(ColorTrackTextView.Direction.LEFT_TO_RIGHT);
+    }
+
+    public void rightToLeft(View view) {
+        changeColor(ColorTrackTextView.Direction.RIGHT_TO_LEFT);
+    }
+
+    private void changeColor(ColorTrackTextView.Direction direction) {
+        colorTrackTextView.setDirection(direction);
+        ValueAnimator valueAnimator = ObjectAnimator.ofFloat(0, 1);
+        valueAnimator.setDuration(2000);
+        valueAnimator.setInterpolator(new DecelerateInterpolator());
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float currentStep = (float) animation.getAnimatedValue();
+                colorTrackTextView.setCurrentProgress(currentStep);
+            }
+        });
+        valueAnimator.start();
     }
 }
--- 自定义view/View_Day01/app/src/main/java/com/wsy/view_day01/view/ColorTrackTextView.java
+++ 自定义view/View_Day01/app/src/main/java/com/wsy/view_day01/view/ColorTrackTextView.java
@@ -12,12 +12,17 @@ import androidx.annotation.Nullable;
 
 import com.wsy.view_day01.R;
 
-public class ColorTrackTextView extends TextView {
+public class ColorTrackTextView extends androidx.appcompat.widget.AppCompatTextView {
     // 1. 实现一个文字两种颜色 - 绘制不变色字体的画笔
     private Paint mOriginPaint;
     // 1. 实现一个文字两种颜色 - 绘制变色字体的画笔
     private Paint mChangePaint;
-    private float currentProgress = 0.5f;
+    private float mCurrentProgress = 0.5f;
+    Direction mDirection = Direction.LEFT_TO_RIGHT;
+
+    public enum Direction {
+        LEFT_TO_RIGHT, RIGHT_TO_LEFT
+    }
 
     // 1. 实现一个文字两种颜色 - 当前的进度
     public ColorTrackTextView(Context context) {
@@ -68,9 +73,8 @@ public class ColorTrackTextView extends TextView {
     }
 
     //利用clipRect 可以裁剪 左边用一个画笔画，右边用另一个画笔去画，不断的改变中间值
-    @Override
+    /*@Override
     protected void onDraw(Canvas canvas) {
-//        super.onDraw(canvas);
         //屏蔽掉原来的onDraw方法
         String text = getText().toString();
         Rect bounds = new Rect();
@@ -81,18 +85,86 @@ public class ColorTrackTextView extends TextView {
         int dy = (int) ((fontMetrics.bottom - fontMetrics.top) / 2 - fontMetrics.bottom);
         int baseLine = getHeight() / 2 + dy;
         //根据进度计算中间值
-        int middle = (int) (currentProgress * getMeasuredWidth());
+        int middle = (int) (currentProgress * getWidth());
 
         canvas.save();
-        Rect rect = new Rect(0, 0, middle, getMeasuredHeight());
+        Rect rect = new Rect(0, 0, middle, getHeight());
         canvas.clipRect(rect);
         canvas.drawText(text, x, baseLine, mChangePaint);
         canvas.restore();
 
         canvas.save();
-        rect = new Rect(middle, 0, getWidth(), getMeasuredHeight());
+        rect = new Rect(middle, 0, getWidth(), getHeight());
         canvas.clipRect(rect);
         canvas.drawText(text, x, baseLine, mOriginPaint);
         canvas.restore();
+    }*/
+
+
+    // 1. 一个文字两种颜色
+    // 利用clipRect的API 可以裁剪  左边用一个画笔去画  右边用另一个画笔去画  不断的改变中间值
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // super.onDraw(canvas);
+        // canvas.clipRect();  裁剪区域
+        // 根据进度把中间值算出来
+        int middle = (int) (mCurrentProgress * getWidth());
+
+        // 从左变到右
+        if (mDirection == Direction.LEFT_TO_RIGHT) {  // 左边是红色右边是黑色
+            // 绘制变色
+            drawText(canvas, mChangePaint, 0, middle);
+            drawText(canvas, mOriginPaint, middle, getWidth());
+        } else {
+            // 右边是红色左边是黑色
+            drawText(canvas, mChangePaint, getWidth() - middle, getWidth());
+            // 绘制变色
+            drawText(canvas, mOriginPaint, 0, getWidth() - middle);
+        }
+    }
+
+
+    /**
+     * 绘制Text
+     *
+     * @param canvas
+     * @param paint
+     * @param start
+     * @param end
+     */
+    private void drawText(Canvas canvas, Paint paint, int start, int end) {
+        canvas.save();
+        // 绘制不变色
+        Rect rect = new Rect(start, 0, end, getHeight());
+        canvas.clipRect(rect);
+        // 我们自己来画
+        String text = getText().toString();
+        Rect bounds = new Rect();
+        paint.getTextBounds(text, 0, text.length(), bounds);
+        // 获取字体的宽度
+        int x = getWidth() / 2 - bounds.width() / 2;
+        // 基线baseLine
+        Paint.FontMetricsInt fontMetrics = paint.getFontMetricsInt();
+        int dy = (fontMetrics.bottom - fontMetrics.top) / 2 - fontMetrics.bottom;
+        int baseLine = getHeight() / 2 + dy;
+        canvas.drawText(text, x, baseLine, paint);// 这么画其实还是只有一种颜色
+        canvas.restore();
+    }
+
+    public void setDirection(Direction direction) {
+        this.mDirection = direction;
+    }
+
+    public void setCurrentProgress(float currentProgress) {
+        this.mCurrentProgress = currentProgress;
+        invalidate();
+    }
+
+    public void setChangeColor(int changeColor) {
+        this.mChangePaint.setColor(changeColor);
+    }
+
+    public void setOriginColor(int originColor) {
+        this.mOriginPaint.setColor(originColor);
     }
 }
