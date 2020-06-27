5c250d9af5fa4d0c4b5ff211b5833a4e3f48e6ac
wsy <wsy1983wsy@163.com>
2020-06-27 16:59:33 +0800

    调整布局

--- 自定义view/View_Day01/app/src/main/java/com/wsy/view_day01/view/taglayout/TagLayout.java
+++ 自定义view/View_Day01/app/src/main/java/com/wsy/view_day01/view/taglayout/TagLayout.java
@@ -33,29 +33,55 @@ public class TagLayout extends ViewGroup {
         int height = getPaddingTop();
         int lineWidth = getPaddingLeft();
         int childCount = getChildCount();
-
+        int originHeight = 0;
         for (int i = 0; i < childCount; i++) {
-            int left;
             //循环子view
             View childView = getChildAt(i);
             //这段话之后就可以获取子View的宽高，因为会调用子View的onMeasure
             measureChild(childView, widthMeasureSpec, heightMeasureSpec);
-            //根据子view计算和指定自己的布局
-
-            if (i == 0 || ((lineWidth + childView.getMeasuredWidth() + getPaddingRight()) > width)) {
-                left = getPaddingLeft();
-                lineWidth = getPaddingLeft() + childView.getMeasuredWidth();
-                height = height + childView.getMeasuredHeight();
+            int left;
+            ViewGroup.MarginLayoutParams layoutParams;
+            if (childView.getLayoutParams() instanceof MarginLayoutParams) {
+                layoutParams = (MarginLayoutParams) childView.getLayoutParams();
             } else {
-                left = lineWidth;
-                lineWidth += childView.getMeasuredWidth();
+                ViewGroup.LayoutParams tmpLayoutParams = childView.getLayoutParams();
+                layoutParams = new ViewGroup.MarginLayoutParams(tmpLayoutParams.width, tmpLayoutParams.height);
+                layoutParams.bottomMargin = layoutParams.topMargin = layoutParams.leftMargin = layoutParams.rightMargin = 0;
             }
-            Rect rect = new Rect(left, height - childView.getMeasuredHeight(), lineWidth, height);
+
+            //根据子view计算和指定自己的布局
+            if (i == 0 || ((lineWidth + childView.getMeasuredWidth() + getPaddingRight() + layoutParams.leftMargin + layoutParams.rightMargin) > width)) {
+                left = getPaddingLeft() + layoutParams.leftMargin;
+                lineWidth = getPaddingLeft() + childView.getMeasuredWidth() + layoutParams.leftMargin + layoutParams.rightMargin;
+                originHeight = height;
+                height = height + childView.getMeasuredHeight() + layoutParams.topMargin + layoutParams.bottomMargin;
+            } else {
+                left = lineWidth + layoutParams.leftMargin;
+                lineWidth += childView.getMeasuredWidth() + layoutParams.leftMargin + layoutParams.rightMargin;
+                height = Math.max(originHeight + childView.getMeasuredHeight() + layoutParams.topMargin + layoutParams.bottomMargin, height);
+            }
+            Rect rect = new Rect(left, height - childView.getMeasuredHeight() - layoutParams.bottomMargin, lineWidth, height);
             childViewRectMap.put(i, rect);
         }
 
         //计算自己的宽高
         setMeasuredDimension(width, height + getPaddingBottom());
+    }
+
+    /**
+     * 重新自定义layoutparams
+     *
+     * @param p
+     * @return
+     */
+    @Override
+    protected LayoutParams generateLayoutParams(LayoutParams p) {
+        return super.generateLayoutParams(p);
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new MarginLayoutParams(getContext(), attrs);
     }
 
     /**
