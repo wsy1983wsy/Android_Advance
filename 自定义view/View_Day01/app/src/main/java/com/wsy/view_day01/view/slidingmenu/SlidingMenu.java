62e73899bf26226102baf354cda5172aee89dc00
wsy <wsy1983wsy@163.com>
2020-06-28 22:05:01 +0800

    菜单缩放

--- 自定义view/View_Day01/app/src/main/java/com/wsy/view_day01/view/slidingmenu/SlidingMenu.java
+++ 自定义view/View_Day01/app/src/main/java/com/wsy/view_day01/view/slidingmenu/SlidingMenu.java
@@ -3,6 +3,7 @@ package com.wsy.view_day01.view.slidingmenu;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.ViewGroup;
 import android.widget.HorizontalScrollView;
 
@@ -63,5 +64,64 @@ public class SlidingMenu extends HorizontalScrollView {
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
         scrollTo(mMenuWidth, 0);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_UP) {
+            //手指抬起
+            int scrollX = getScrollX();
+            //向左移动超过了菜单的1/2，则认为需要关闭
+            if (scrollX > (mMenuWidth / 2)) {
+                //关闭
+                closeMenu();
+            } else {
+                //打开
+                openMenu();
+            }
+            return true;
+        }
+        return super.onTouchEvent(ev);
+    }
+
+    /**
+     * 关闭菜单，滚动到menuWidth位置
+     */
+    private void closeMenu() {
+        smoothScrollTo(mMenuWidth, 0);
+    }
+
+    /**
+     * 打开菜单，滚动到0
+     */
+    private void openMenu() {
+        smoothScrollTo(0, 0);
+    }
+
+    //4. 处理有点的缩放，左边的缩放和透明度，需要不断的获取当前滚动的位置
+
+
+    @Override
+    protected void onScrollChanged(int left, int t, int oldl, int oldt) {
+        super.onScrollChanged(left, t, oldl, oldt);
+        //算一个梯度值
+        float scale = 1f * left / mMenuWidth;
+        //右边的缩放，最小时0.7，最大时1.0f
+        float rightScale = 0.7f + 0.3f * scale;
+        //设置中心点
+        contentContainer.setPivotX(0);
+        contentContainer.setPivotY(contentContainer.getHeight() / 2);
+        contentContainer.setScaleX(rightScale);
+        contentContainer.setScaleY(rightScale);
+
+        //菜单的缩放和透明度
+        //透明是由半透明到完全透明 0.7 到 1.0
+        float leftAlpha = 0.5f + (1 - scale) * 0.5f;
+        menuContainer.setAlpha(leftAlpha);
+        //缩放是由0.7到1.0
+        float leftScale = 0.7f + (1 - scale) * 0.3f;
+        menuContainer.setTranslationX(left * leftScale);
+        menuContainer.setScaleX(leftAlpha);
+        menuContainer.setScaleY(leftAlpha);
     }
 }
