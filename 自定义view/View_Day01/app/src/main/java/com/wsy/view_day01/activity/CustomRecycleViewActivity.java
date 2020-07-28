47adfb4ad89a025000b0db217f5dba7709093504
wsy <wsy1983wsy@163.com>
2020-07-28 09:01:17 +0800

    tmp save

---
+++ 自定义view/View_Day01/app/src/main/java/com/wsy/view_day01/activity/CustomRecycleViewActivity.java
@@ -0,0 +1,13 @@
+package com.wsy.view_day01.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class CustomRecycleViewActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
