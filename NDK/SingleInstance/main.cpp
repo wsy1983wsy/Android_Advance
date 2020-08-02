8d1ddae1465e9680d04eb475145d361e152405d4
wsy <wsy1983wsy@163.com>
2020-08-02 17:45:43 +0800

    模板

--- NDK/SingleInstance/main.cpp
+++ NDK/SingleInstance/main.cpp
@@ -2,7 +2,24 @@
 #include "Student.h"
 #include "Test.h"
 #include "Parent.h"
+//函数模板  对应于java中的泛型方法
+template<typename T>
+T max(T i, T j) {
+  return i > j ? i : j;
+}
+template<typename T>
+T add(T i, T j) {
+  return i + j;
+}
 
+//类模板  对应于java中的泛型类
+template<class T, class E>
+class Q {
+ public:
+  E test(T t, E e) {
+    return t + e;
+  }
+};
 int main() {
   Student* student = Student::getInstance();
   std::cout << student->getI() << std::endl;
@@ -27,5 +44,9 @@ int main() {
   Parent1* child4 = new Child();
   child4->test();
 
+  std::cout << add<float>(10, 15.5) << std::endl;
+
+  Q<int, float> q;
+  std::cout << q.test(10, 1.2) << std::endl;
   return 0;
 }
