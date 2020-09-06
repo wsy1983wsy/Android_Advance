aea4a0ecf361b42b9ebb9c5a51446e3b4ae1bc1c
wsy <wsy1983wsy@163.com>
2020-09-06 18:57:31 +0800

    模拟OKHttp流程

---
+++ OKHttp/custom_okhttp/src/main/java/com/wsy/custom/okhttp/okhttp/Call2.java
@@ -0,0 +1,5 @@
+package com.wsy.custom.okhttp.okhttp;
+
+public interface Call2 {
+    void enqueue(Callback2 responseCallback);
+}
---
+++ OKHttp/custom_okhttp/src/main/java/com/wsy/custom/okhttp/okhttp/Response2.java
@@ -0,0 +1,18 @@
+package com.wsy.custom.okhttp.okhttp;
+
+public class Response2 {
+
+    private String body;
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String string() {
+        return body;
+    }
+}
